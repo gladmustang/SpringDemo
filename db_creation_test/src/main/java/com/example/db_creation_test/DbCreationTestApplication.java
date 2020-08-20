0857d618f91473699785d82ff48b3480569c1671
Jason Shen <jshen103@nam.ad.pwcinternal.com>
2020-08-20 11:10:32 +0800

    fix pom

---
+++ db_creation_test/src/main/java/com/example/db_creation_test/DbCreationTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.db_creation_test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbCreationTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbCreationTestApplication.class, args);
+    }
+
+}
---
+++ db_creation_test/src/test/java/com/example/db_creation_test/DbCreationTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.db_creation_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbCreationTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
